fd76018a8fe2ba1a825d022b55bf24e88f84285c
Rav [RedChamps] <rav@redchamps.com>
2020-01-28 11:36:01 +0530

    Fixed reported issue https://github.com/magento/magento2/issues/26437

--- Rewrite/Customer/Block/Adminhtml/Edit/Tab/Cart.php
+++ Rewrite/Customer/Block/Adminhtml/Edit/Tab/Cart.php
@@ -1,8 +1,32 @@
 <?php
 namespace RedChamps\CustomerCarts\Rewrite\Customer\Block\Adminhtml\Edit\Tab;
 
+use Magento\Backend\Block\Widget\Grid\Extended;
+use Magento\Framework\Exception\NoSuchEntityException;
+
 class Cart extends \Magento\Customer\Block\Adminhtml\Edit\Tab\Cart
 {
+    /**
+     * Added fix when customer do not have any active quote
+     * Magento 2 issue: https://github.com/magento/magento2/issues/26437
+     * Pull request: https://github.com/magento/magento2/pull/26489
+     */
+    protected function _prepareCollection()
+    {
+        $quote = $this->getQuote();
+
+        if ($quote && $quote->getId()) {
+            $collection = $quote->getItemsCollection(false);
+            $collection->addFieldToFilter('parent_item_id', ['null' => true]);
+        } else {
+            $collection = $this->_dataCollectionFactory->create();
+        }
+
+        $this->setCollection($collection);
+
+        return Extended::_prepareCollection();
+    }
+
     protected function getQuote()
     {
         if (null === $this->quote) {
@@ -11,7 +35,7 @@ class Cart extends \Magento\Customer\Block\Adminhtml\Edit\Tab\Cart
 
             try {
                 $this->quote = $this->quoteFactory->create()->setSharedStoreIds($storeIds)->loadByCustomer($customerId);
-            } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+            } catch (NoSuchEntityException $e) {
                 $this->quote = $this->quoteFactory->create()->setSharedStoreIds($storeIds);
             }
         }
